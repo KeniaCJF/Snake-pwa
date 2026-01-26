eea173383dda24f9d7ba5c6fd5f13125e345ae97
KeniaCJF~ <keniacarolina.j@gmail.com>
2026-01-25 22:34:40 -0600

    personalizacion

--- game.js
+++ game.js
@@ -1,6 +1,11 @@
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 const scoreEl = document.getElementById("score");
+const carrotImg = new Image();
+carrotImg.src = "images/Carrot.png";
+const bunnyImg = new Image();
+bunnyImg.src = "images/Bunny.png";
+
 
 let snake = [{ x: 150, y: 150 }];
 let food = { x: 60, y: 60 };
@@ -8,16 +13,20 @@ let dx = 10;
 let dy = 0;
 let score = 0;
 
+function changeDirection(x,y){
+dx = x;
+dy = y;
+}
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   // Dibujar comida
-  ctx.fillStyle = "red";
-  ctx.fillRect(food.x, food.y, 10, 10);
+ctx.drawImage(carrotImg, food.x, food.y, 30, 30);
 
   // Dibujar serpiente
-  ctx.fillStyle = "lime";
-  snake.forEach(part => ctx.fillRect(part.x, part.y, 10, 10));
+snake.forEach(part => {
+  ctx.drawImage(bunnyImg, part.x, part.y, 30, 30);
+});
 
   // Mover serpiente
   const head = { x: snake[0].x + dx, y: snake[0].y + dy };
@@ -51,10 +60,27 @@ function draw() {
 }
 
 document.addEventListener("keydown", e => {
-  if (e.key === "ArrowUp") { dx = 0; dy = -10; }
-  if (e.key === "ArrowDown") { dx = 0; dy = 10; }
-  if (e.key === "ArrowLeft") { dx = -10; dy = 0; }
-  if (e.key === "ArrowRight") { dx = 10; dy = 0; }
+  if (e.key === "ArrowUp") changeDirection(0, -10);
+  if (e.key === "ArrowDown") changeDirection(0, 10);
+  if (e.key === "ArrowLeft") changeDirection(-10, 0);
+  if (e.key === "ArrowRight") changeDirection(10, 0);
 });
+
+
+function directionbutton(id, x, y) {
+  const btn = document.getElementById(id);
+  if (!btn) return;
+
+  btn.addEventListener("click", () => {
+    changeDirection(x, y);
+  });
+}
+
+
+directionbutton("up", 0,-10);
+directionbutton("down",0,10);
+directionbutton("left",-10,0);
+directionbutton("right",10,0);
+
 
 setInterval(draw, 100);
